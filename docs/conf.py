440633b9a8471540aecca385d8ec74036cc355f9
YuandongMa <90610597+YuandongMa@users.noreply.github.com>
2021-10-22 10:32:24 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'pet教程'
+project = 'MMDtection'
 copyright = '2018-2021, OpenMMLab'
 author = 'MMDetection Authors'
 version_file = '../mmdet/version.py'
