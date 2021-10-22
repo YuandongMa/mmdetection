615810d346e9eac90a289aef2df29f74a86503e9
YuandongMa <90610597+YuandongMa@users.noreply.github.com>
2021-10-22 14:55:52 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ html_theme_options = {
     'menu': [
         {
             'name': 'GitHub',
-            'url': 'https://github.com/open-mmlab/mmdetection'
+            'url': 'https://www.baidu.com/'
         },
         {
             'name':
